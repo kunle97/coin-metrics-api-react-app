6635b1cebbcf7f543696fc8b082b5c7907f9eb70
kunle <adekunle.ashton@gmail.com>
2022-12-05 20:44:27 -0500

    initial commit redering all assets from the coin metrics community API in column 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./bootstrap/css/bootstrap.css";
+import AssetListItem from "./components/AssetListItem";
+// import "./bootstrap/js/bootstrap"
 
 function App() {
+  const [validAssets, setValidAssets] = useState([]);
+  const [activeMetrics, setActiveMetrics] = useState([]);
+   useEffect(() => {
+    getAssetData();
+  }, []);
+  const getAssetData = () => {
+    fetch("https://community-api.coinmetrics.io/v4/catalog/assets")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(typeof data);
+        //TODO: Set the valid assets that have at least one metric
+        setValidAssets(data.data);
+        console.log("Valid Assets", validAssets);
+      });
+  };
+  const getMetrius = () => {
+    fetch("https://community-api.coinmetrics.io/v4/catalog-all/asset-metrics")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(typeof data);
+        //TODO: Set the valid assets that have at least one metric
+        setActiveMetrics(data.data);
+        console.log("Valid Assets", validAssets);
+      });
+  };
+
+  const searchAsset = () => {
+    //Live update assets when search box is changed
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="form-group"></div>
+      <div className="row">
+        <div className="col-md-6 asset-list-container">
+          <input
+            onChange={searchAsset}
+            className="form-control"
+            placeholder="Search for an asset"
+          />
+          <ul className="asset-list data-list">
+            {validAssets &&
+              validAssets.map((asset) => {
+                return (
+                  <AssetListItem
+                    full_name={asset.full_name}
+                    asset={asset.asset}
+                  />
+                );
+              })}
+          </ul>
+        </div>
+        <div className="col-md-6 metric-list-container">
+          {" "}
+          <li className="metric-list-item">
+            <h4 className="metric-title">MArket Cap </h4>
+            <p>Metric description</p>
+          </li>
+        </div>
+      </div>
+      <button className="btn btn-primary" onClick={getAssetData}>
+        Load Data
+      </button>
     </div>
   );
 }
---
+++ src/components/AssetListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function AssetListItem(props) {
+  return (
+    <li className="asset-list-item">
+      <h4 className="asset-title">{props.full_name} ({props.asset})</h4>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-chevron-right asset-list-icon"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+        />
+      </svg>
+    </li>
+  );
+}
+
+export default AssetListItem;
