2336a7f7bb39fc5ef922ea814e951d235b488f56
kunle <adekunle.ashton@gmail.com>
2022-12-07 22:57:58 -0500

    updated header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,10 @@ const Header = (props) => {
       <p>
         Select an assset to see its corresponding metrics. Select a metric to
         see its corresponding assets. You may deselect an item to reset the
-        other list. You may also search for either in thier respective search
-        bars. You may use the reset button at the bottom to restore all items to
-        each list.
+        other list. (Works best when you deleselect a currently selected item in a given list 
+        then select another item to refresh the opposite list automatically) You may
+        also search for either in thier respective search bars. You may use 
+        the reset button at the bottom to restore all items to each list.
       </p>
     </div>
   );
